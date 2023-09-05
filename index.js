8c5540ceaa0b31b865947d4c4b2c2ca764841697
Santiago Crippa <santicrippa@gmail.com>
2023-09-05 16:31:34 -0300

    product manager

---
+++ index.js
@@ -0,0 +1,34 @@
+// Realizar una clase "ProductManager" que gestione un conjunto de productos. 
+// Debe crearse dedde su constructor con el elemento products, el cual sera un array vacio.
+// Cada producto que gestione debe contar con las sig. props:  title,description,price,thumbnail(url),code,stock
+// Metodos a agregar: 
+// addProduct: agregara un prod al arreglo de productos. Validar que no se repita el campo "code" y que todos los campos sean obligatorios. Al agregarlo, debe crearse con un id autoincrementable
+// getProducts: debe devolver un array con todos los prods creados hasta ese momento.
+// getProductById: debe buscar en el array el prod que coincida con el id. En caso de no coincidir ningun id, mostrar en consola, "error not found". 
+
+class ProductManager {
+    constructor() {
+        this.products = []
+    }
+    addProduct(title, description, price, thumbnail, code, stock) {
+        let id
+        if (!this.products.length) {
+            id = 1
+        } else {
+            
+        }
+        const product = {
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        return product
+    }
+}
+const manager = new ProductManager
+console.log(manager);
