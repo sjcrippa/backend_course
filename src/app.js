79dfdbcdec93ff81cc5878dbdd3a8ab215f9eccf
Santiago Crippa <santicrippa@gmail.com>
2023-11-17 18:36:06 -0300

    buiilding product manager

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
-class productManager {
+class ProductManager {
   constructor() {
     this.products = []
+    this.id = 1
   }
-  addProduct() {
-    this.title = title 
-    this.description = description 
-    this.price = price 
-    this.thumbnail = thumbnail 
-    this.stock = stock 
-    this.code = code 
+
+  addProduct(title, description, price, thumbnail, stock, code) {
+    const newProduct = {
+      id: this.id++,
+      title,
+      description,
+      price,
+      thumbnail,
+      stock,
+      code
+    }
+    this.products.push(newProduct)
+  }
+
+  getProducts() {
+    console.log(this.products);
+  }
+
+  getProcutsById() {
+
   }
 }
 
-const products = new productManager()
-console.log(products);
+const manager = new ProductManager()
+manager.addProduct('Title', 'Description', 123, 'url', 1, 'code')
+manager.addProduct('Title', 'Description', 123, 'url', 1, 'code')
+manager.getProducts()
