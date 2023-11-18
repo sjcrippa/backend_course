8364126f817f7531aab062be7d7cbdfc330e905b
Santiago Crippa <santicrippa@gmail.com>
2023-11-18 17:49:42 -0300

    product manager

--- src/app.js
+++ src/app.js
@@ -4,35 +4,46 @@ class ProductManager {
     this.id = 1
   }
 
-  addProduct(title, description, price, thumbnail, stock, code) {
+  addProduct(title, description, price, thumbnail, code, stock) {
     //validacion para que todos los productos sean obligatorios:
-    if(!title || !description || !stock) {
-      console.log('todos los campos son obligatorios');
-      return
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      return console.log('All fields are required');
     }
+
+    // validacion para que no se repita el code:
+    const productExist = this.products.find(prod => prod.code === code)
+    if (productExist) return console.log(`Product with the code ${code} already exist`);
+
     const newProduct = {
       id: this.id++,
       title,
       description,
       price,
       thumbnail,
-      stock,
-      code
+      code,
+      stock
     }
     this.products.push(newProduct)
   }
 
   getProducts() {
-    console.log(this.products);
+    return this.products
   }
 
-  getProcutsById() {
-
+  getProductsById(idProduct) {
+    const productById = this.products.find(prod => prod.id === idProduct)
+    if (productById) return console.log(productById);
+    if (!productById) return console.log('Product not found');
   }
 }
 
 const manager = new ProductManager()
-manager.addProduct('Caca', 'pedo', 123, 'url', 1, 'code')
-manager.addProduct('Title', 'Description', 123, 'url', 1, 'code')
-manager.addProduct('Title', 'Description', 123, 'url', 'code')
+manager.addProduct('T-shirt', 'Blue', 250, 'url/of/product', 'AAA001', 1)
+manager.addProduct('Hoodie', 'Red', 170, 'url/of/product', 'AAA002', 1)
 manager.getProducts()
+
+manager.addProduct('Pants', 'Yellow', 320, 'url/of/product', 'AAA002', 1)
+manager.addProduct('Boxes', 'Yellow', 320, 'url/of/product', 'AAA003', 1)
+manager.getProducts()
+
+manager.getProductsById(2)
