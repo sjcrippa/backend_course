e8cc9ca0137e6c988a2315852fb286bcbfe947b5
Santiago Crippa <santicrippa@gmail.com>
2023-12-06 00:17:38 -0300

    class 6

--- src/clases/05/05.js
+++ src/clases/05/05.js
@@ -1,12 +1,21 @@
-const UserManager = require('./class/user-manager.class')
+import UserManager from "./class/user-manager.class.js"
 
 const userManager = new UserManager()
 
-const newUser = {
-  name: 'Jhon',
-  lastname: 'Doe',
-  username: 'jhondoe',
-  password: 'doe123'
+const users = async () => {
+
+  const newUser = {
+    name: 'Jhon',
+    lastname: 'Doe',
+    username: 'jhondoe',
+    password: 'doe123'
+  }
+
+  userManager.createUser(newUser)
+  const users = await userManager.getUsers()
+  console.log(users);
+
+  userManager.validateUser('jhondoe', 'doe123')
 }
 
-userManager.createUser(newUser)
+users()
--- src/clases/05/class/user-manager.class.js
+++ src/clases/05/class/user-manager.class.js
@@ -1,11 +1,22 @@
 // handsOfLabe clase 5
-const fs = require('node:fs')
-const crypto = require('node:crypto')
+import fs from 'node:fs'
+import crypto from 'node:crypto'
 
 const path = './files/Users.json'
 
 class UserManager {
   users = [];
+
+  async getUsers() {
+    if (fs.existsSync(path)) {
+      const data = await fs.promises.readFile(path, 'utf-8')
+      const users = JSON.parse(data)
+
+      return users
+    }
+
+    console.log('El archivo no existe');
+  }
 
   async createUser(user) {
     const salt = crypto.randomBytes(128).toString('base64')
@@ -23,13 +34,37 @@ class UserManager {
 
     this.users.push(newUser)
 
-    await fs.promises.mkdir('./files', {recursive:true})
+    await fs.promises.mkdir('./files', { recursive: true })
     await fs.promises.writeFile(path, JSON.stringify(this.users, null, '\t'))
 
-  }
-  validateUser() {
+    console.log('User created');
 
+  }
+
+  async validateUser(username, password) {
+    const users = await this.getUsers()
+
+    if (!users) {
+      return console.log('No hay usuarios');
+    }
+    
+    const userToValidate = users.find(user => user.username === username)
+
+    if (!userToValidate){
+      return console.log('Usuario no encontrado');
+    }
+
+    const hashedPassword = crypto
+      .createHmac('sha256', userToValidate.salt)
+      .update(password)
+      .digest('hex')
+
+    if (hashedPassword === userToValidate.password) {
+      console.log('Logueado correctamente');
+    } else {
+      console.log('Error, contraseña incorrecta');
+    }
   }
 }
 
-module.exports = UserManager
+export default UserManager
